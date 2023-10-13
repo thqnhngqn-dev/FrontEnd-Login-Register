c6cc4df5466f359cfb8bcdddbd5a8ddc1366510c
thqnhngqn-dev <thqnhngqn.dev@gmail.com>
2023-10-13 11:01:09 +0700

    first commit

---
+++ access/javascript/index.js
@@ -0,0 +1,22 @@
+//methods and events click form Register
+function displayRegister() {
+    document.getElementById("mySignup").addEventListener("click", function() {
+        window.location.href = "Register.html";
+    });
+}
+// methods and events click form Login
+function displayLogin(){
+    document.getElementById('myLogin').addEventListener("click", function(){
+        window.location.href = 'Login.html';
+    });
+}
+function Login(){
+    document.getElementById('btnLogin').addEventListener('', function(){
+        window.location.href = '';
+    });
+}
+function Register(){
+    document.getElementById('btnRegister').addEventListener('', function(){
+        window.location.href = '';
+    });
+}
